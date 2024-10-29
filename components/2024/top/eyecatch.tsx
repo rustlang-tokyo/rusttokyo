852532b6a949bbb5ecaf338d2a6c738aaf969a5d
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-10-29 20:09:14 +0900

    チケット購入ボタンの修正

--- components/2024/top/eyecatch.tsx
+++ components/2024/top/eyecatch.tsx
@@ -29,15 +29,13 @@ export function Eyecatch(props: {
         <Text fontSize="sm">{props.textSource.dateText}</Text>
       </Box>
       <Box mt="20px">
-        <Button as="a" href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Rust.Tokyo+2024&details=The+Rust+Conference+happening+offline+and+online!&dates=20241130T110000/20241130T165000&ctz=Asia/Tokyo&location=Shibuya+FUKURAS%2C+1-ch%C5%8Dme-2-3+D%C5%8Dgenzaka%2C+Shibuya+City%2C+Tokyo+150-0043%2C+Japan" leftIcon={<BiCalendarPlus />} width={275.4}>
+        <Button as="a" target="_blank" href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Rust.Tokyo+2024&details=The+Rust+Conference+happening+offline+and+online!&dates=20241130T110000/20241130T165000&ctz=Asia/Tokyo&location=Shibuya+FUKURAS%2C+1-ch%C5%8Dme-2-3+D%C5%8Dgenzaka%2C+Shibuya+City%2C+Tokyo+150-0043%2C+Japan" leftIcon={<BiCalendarPlus />} width={275.4}>
           {props.textSource.addCalendarText}
         </Button>
       </Box>
       <Box mt="20px">
-        <Button leftIcon={<RiTicket2Line />} width={275.4}>
-          <Link href="https://ti.to/rust-tokyo/2024" isExternal>
-            {props.textSource.purchaseTicketText}
-          </Link>
+        <Button as="a" target="_blank" href="https://ti.to/rust-tokyo/2024" leftIcon={<RiTicket2Line />} width={275.4}>
+          {props.textSource.purchaseTicketText}
         </Button>
       </Box>
       {/* <Box mt="20px">
