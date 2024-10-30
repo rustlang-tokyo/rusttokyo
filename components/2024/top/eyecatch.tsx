bab7765b4c8fb9689f8d5f47d81294ea85d9d769
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2024-10-30 16:33:30 +0900

    ビルドエラーが起きないようにしゅうせい

--- components/2024/top/eyecatch.tsx
+++ components/2024/top/eyecatch.tsx
@@ -1,15 +1,14 @@
 import type { EnTopTextList, JaTopTextList } from "@/constants/2024/top/texts";
 import {
   Box,
-  Button,
   Center,
   Container,
   Img,
-  Link,
   Text,
 } from "@chakra-ui/react";
 import { BiCalendarPlus } from "react-icons/bi";
 import { RiTicket2Line } from "react-icons/ri";
+import { LinkButton } from "./link_button";
 
 export function Eyecatch(props: {
   isPc: boolean;
@@ -29,14 +28,20 @@ export function Eyecatch(props: {
         <Text fontSize="sm">{props.textSource.dateText}</Text>
       </Box>
       <Box mt="20px">
-        <Button as="a" rel="noreferrer noopener" target="_blank" href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Rust.Tokyo+2024&details=The+Rust+Conference+happening+offline+and+online!&dates=20241130T110000/20241130T165000&ctz=Asia/Tokyo&location=Shibuya+FUKURAS%2C+1-ch%C5%8Dme-2-3+D%C5%8Dgenzaka%2C+Shibuya+City%2C+Tokyo+150-0043%2C+Japan" leftIcon={<BiCalendarPlus />} width={275.4}>
+        <LinkButton
+          leftIcon={<BiCalendarPlus />}
+          href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Rust.Tokyo+2024&details=The+Rust+Conference+happening+offline+and+online!&dates=20241130T110000/20241130T165000&ctz=Asia/Tokyo&location=Shibuya+FUKURAS%2C+1-ch%C5%8Dme-2-3+D%C5%8Dgenzaka%2C+Shibuya+City%2C+Tokyo+150-0043%2C+Japan"
+        >
           {props.textSource.addCalendarText}
-        </Button>
+        </LinkButton>
       </Box>
       <Box mt="20px">
-        <Button as="a" rel="noreferrer noopener" target="_blank" href="https://ti.to/rust-tokyo/2024" leftIcon={<RiTicket2Line />} width={275.4}>
+        <LinkButton
+          href="https://ti.to/rust-tokyo/2024"
+          leftIcon={<RiTicket2Line />}
+        >
           {props.textSource.purchaseTicketText}
-        </Button>
+        </LinkButton>
       </Box>
       {/* <Box mt="20px">
         <Button leftIcon={<RiTicket2Line />} width={275.4}>
