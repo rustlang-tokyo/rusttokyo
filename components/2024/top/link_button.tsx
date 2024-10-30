a1e4654b7245049ce8748d8e01b6cf49a73bf807
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2024-10-30 21:00:09 +0900

    Feature/2024におけるTOPページのリンクボタン修正 (#9)
    * カレンダー追加ボタンの修正
    
    * チケット購入ボタンの修正
    
    * タブナビングの防止のためのrel属性の追加
    
    * ビルドエラーが起きないようにしゅうせい

---
+++ components/2024/top/link_button.tsx
@@ -0,0 +1,24 @@
+import {Button, ButtonOptions} from "@chakra-ui/react"
+
+export function LinkButton({
+    href,
+    leftIcon,
+    children
+}: {
+    href: string,
+    leftIcon: ButtonOptions["leftIcon"]
+    children: React.ReactNode
+}) {
+    return (
+        <Button
+            as="a"
+            rel="noreferrer noopener"
+            target="_blank"
+            href={href}    
+            leftIcon={leftIcon}
+            width={275.4}
+        >
+            {children}
+        </Button>
+    )
+}
