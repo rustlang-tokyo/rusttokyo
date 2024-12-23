6716f843cbd6fd95787cd6844c1563224815f97a
dorayakikun <6278699+dorayakikun@users.noreply.github.com>
2024-12-23 16:47:37 +0900

    fix: Hydration Error を解消

--- components/2024/lineup/session_description.tsx
+++ components/2024/lineup/session_description.tsx
@@ -18,13 +18,17 @@ import {
   Wrap,
   WrapItem,
 } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { FaClock, FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { MdPlace } from "react-icons/md";
 import { SiQiita } from "react-icons/si";
-import ReactPlayer from "react-player";
 import { TwitterShareButton } from "react-share";
 import { PresentationMaterials } from "./materials";
+
+const ReactPlayer = dynamic(() => import("react-player/youtube"), {
+  ssr: false,
+});
 
 export function SessionDescription(props: {
   isPc: boolean;
